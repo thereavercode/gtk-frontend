59056a3ff10d10fb2fe00815ce34514c3b79290a
theeavercode <giftiwjr@gmail.com>
2025-06-30 20:57:11 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import PaymentForm from "./components/PaymentForm";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <PaymentForm />
+      <SpeedInsights />
     </div>
   );
 }
