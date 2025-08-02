4b8eb548083fad208594c4e677fde587be5a81ab
thereavercode <45076411+thereavercode@users.noreply.github.com>
2025-08-02 10:08:46 +0000

    declaraions.d.ts

---
+++ src/declarations.d.ts
@@ -0,0 +1,3 @@
+// src/declarations.d.ts
+declare module "@fontsource/inter";
+declare module "@fontsource/inter/variable.css";
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import "@fontsource/inter";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "@fontsource/inter/variable.css"; // At the top of your app entry point
 import './index.css'
 import App from './App.tsx'
 
