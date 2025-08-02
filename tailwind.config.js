31e06ca9adb7ef615a5d7c3d1ac87bada0dc0807
thereavercode <45076411+thereavercode@users.noreply.github.com>
2025-08-02 10:48:08 +0000

    white-5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-5': 'rgba(255, 255, 255, 0.05)',
+        'white-10': 'rgba(255, 255, 255, 0.1)',
+      },
+    },
   },
-  plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 };
